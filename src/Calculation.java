e8ec10488f86c118458aef8abe399227452fef93
Anuj Kumar <cdac.anuj@gmail.com>
2020-06-13 18:01:39 +0530

    implemented divide function

--- src/Calculation.java
+++ src/Calculation.java
@@ -6,10 +6,15 @@ public class Calculation {
 
         calc = new Calculation();
         System.out.println("Add:" + calc.add(2, 3));
+        System.out.println("Divide:" + calc.divide(6, 3));
     }
 
     private int add(int x, int y) {
         return x + y;
+    }
+
+    private int divide(int x, int y) {
+        return x / y;
     }
 
 }
